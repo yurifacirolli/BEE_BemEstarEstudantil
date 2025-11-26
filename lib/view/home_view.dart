8ffaaa7297d0143c2e7dd99865bbe6fcded67baf
yurifacirolli <yuri.facirolli@gmail.com>
2025-11-26 17:55:55 -0300

    Documentação

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 import 'package:app_project/controller/home_controller.dart';
 
